547f1c9b5e793611005398f5a26b245154d72f6e
pimvanhespen <pimvanhespen@gmail.com>
2016-11-10 13:48:35 +0100

    `todo's weggehaald

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ def flashFunction(maxRange, offset):
             GPIO.output(ALARM_PORT, x < offset)
             time.sleep(0.005)
 
+
 while not GPIO.input(WINDOW_PORT):
     for offset in range(MAX_STEPS):
         flashFunction(MAX_STEPS, offset)
@@ -52,18 +53,16 @@ def flashing():
                 flashFunction(MAX_STEPS, offset)
         time.sleep(0.05)
 
+
 flash_thread = threading.Thread(target=flashing)
 flash_thread.start()
 
 try:
     while running:  # MAIN while-loop, checks for program state.
-        if state == State.IDLE:  # IDLE State means detecting if someone 'broke in'
-            if GPIO.input(WINDOW_PORT):
-                state = State.ALARM
-                GPIO.output(ALARM_PORT, True)
-                isFlashing = True
-                # TODO: time the last time we switched the alarm on, call it timeSinceLastSwitch
-                # TODO: compare timeSinceLastSwitch to currTime, if greater than set e.g. 1 second switch light on/off
+        if state == State.IDLE and GPIO.input(WINDOW_PORT):  # IDLE State means detecting if someone 'broke in'
+            state = State.ALARM
+            GPIO.output(ALARM_PORT, True)
+            isFlashing = True
         # Else, the state is ALARM or LOGIN. This means we wait for login press to enter password.
         elif GPIO.input(LOGIN_PORT):
            state = State.LOGIN
