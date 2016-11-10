28656bfa48b3cf449a2ffdbbe5c4a89d5e8b9af8
pimvanhespen <pimvanhespen@gmail.com>
2016-11-10 12:48:52 +0100

    fred toegevoegd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO
+import threading
+import time
 
 # constants
 WINDOW_PORT = 11  # GPIO port for the 'window' (a.k.a. alarm initiation)
@@ -6,6 +8,8 @@ ALARM_PORT = 13  # port for alarm(s)
 LOGIN_PORT = 15  # port for button to initiate login protocol
 LOGIN_PASS = 'jemoeder'  # password for login
 
+TIME_PER_STEP = 10
+MAX_STEPS = 100
 
 class State:
     IDLE = 0
@@ -13,35 +17,64 @@ class State:
     LOGIN = 2
 
 
-class Main:
-    def __init__(self):
-        # initiating GPIO header
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(WINDOW_PORT, GPIO.IN)
-        GPIO.setup(ALARM_PORT, GPIO.OUT)
-        GPIO.setup(LOGIN_PORT, GPIO.IN)
-        state = State.IDLE
-        self.running = True
-        try:
-            while self.running:  # MAIN while-loop, checks for program state.
-                if state == State.IDLE:  # IDLE State means detecting if someone 'broke in'
-                    if GPIO.input(WINDOW_PORT):
-                        state = State.ALARM
-                        GPIO.output(ALARM_PORT, True)
-                        # TODO: time the last time we switched the alarm on, call it timeSinceLastSwitch
-                else:  # Else, the state is ALARM or LOGIN. This means we wait for login press to enter password.
-                    # TODO: compare timeSinceLastSwitch to currTime, if greater than set e.g. 1 second switch light on/off
-                    if GPIO.input(LOGIN_PORT):
-                        state = State.LOGIN
-                    if state == State.LOGIN:
-                        if not LOGIN_PASS == input('Voer uw wachtwoord in: '):
-                            print('Uw wachtwoord klopt niet.')
-                            while not LOGIN_PASS == input('Voer uw wachtwoord in: '):
-                                print('Uw wachtwoord klopt niet.')
-                        # After this while loop we have a valid password.
-                        state = State.IDLE
-                        GPIO.output(ALARM_PORT, False)
-        finally:
-            GPIO.cleanup()
-Main()
+# initiating GPIO header
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(WINDOW_PORT, GPIO.IN)
+GPIO.setup(ALARM_PORT, GPIO.OUT)
+GPIO.setup(LOGIN_PORT, GPIO.IN)
+state = State.IDLE
+
+isFlashing = False
+
+running = True
+
+def flashFunction(maxRange, offset):
+    for looping in range(TIME_PER_STEP):
+        for x in range(maxRange):
+            GPIO.output(ALARM_PORT, x < offset)
+            time.sleep(0.005)
+
+while not GPIO.input(WINDOW_PORT):
+    for offset in range(MAX_STEPS):
+        flashFunction(MAX_STEPS, offset)
+    for offset in reversed(range(MAX_STEPS)):
+        flashFunction(MAX_STEPS, offset)
+
+
+def flashing():
+    """Thread flashing function"""
+    while True:
+        while isFlashing:
+            for offset in range(MAX_STEPS):
+                flashFunction(MAX_STEPS, offset)
+            for offset in reversed(range(MAX_STEPS)):
+                flashFunction(MAX_STEPS, offset)
+        time.sleep(0.05)
+
+flash_thread = threading.Thread(target=flashing)
+flash_thread.start()
+
+try:
+    while running:  # MAIN while-loop, checks for program state.
+        if state == State.IDLE:  # IDLE State means detecting if someone 'broke in'
+            if GPIO.input(WINDOW_PORT):
+                state = State.ALARM
+                GPIO.output(ALARM_PORT, True)
+                isFlashing = True
+                # TODO: time the last time we switched the alarm on, call it timeSinceLastSwitch
+                # TODO: compare timeSinceLastSwitch to currTime, if greater than set e.g. 1 second switch light on/off
+        # Else, the state is ALARM or LOGIN. This means we wait for login press to enter password.
+        elif GPIO.input(LOGIN_PORT):
+           state = State.LOGIN
+        elif state == State.LOGIN:
+            if not LOGIN_PASS == input('Voer uw wachtwoord in: '):
+                print('Uw wachtwoord klopt niet.')
+                while not LOGIN_PASS == input('Voer uw wachtwoord in: '):
+                    print('Uw wachtwoord klopt niet.')
+            # After this while loop we have a valid password.
+            state = State.IDLE
+            isFlashing = False
+finally:
+    GPIO.cleanup()
+
